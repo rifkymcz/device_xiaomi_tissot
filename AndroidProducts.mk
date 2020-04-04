fc318f1e478363ea740f279772a3746af9e69e7f
Rifky Mcz <32016630+rifkymcz@users.noreply.github.com>
2020-04-04 15:52:00 +0800

    tissot: komodo-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_tissot.mk
+    $(LOCAL_DIR)/komodo_tissot.mk
+
+COMMON_LUNCH_CHOICES := \
+    komodo_tissot-user \
+    komodo_tissot-userdebug \
+    komodo_tissot-eng
