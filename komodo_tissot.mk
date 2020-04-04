44db230107c4f407e40549cafa368d959cbe2f6e
Rifky Mcz <32016630+rifkymcz@users.noreply.github.com>
2020-04-04 15:47:06 +0800

    tissot: komodo-ify

--- komodo_tissot.mk
+++ komodo_tissot.mk
@@ -21,8 +21,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/tissot/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common KomodoOS stuff.
+$(call inherit-product, vendor/komodo/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
@@ -32,6 +32,9 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
+TARGET_BOOT_ANIMATION_RES := 1080
+KOMODO_OFFICIAL=true
+CURRENT_BUILD_TYPE := nogapps
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
@@ -40,7 +43,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
-
-# PixelExperience Stuffs
-TARGET_GAPPS_ARCH := arm64
-TARGET_BOOT_ANIMATION_RES := 1080
